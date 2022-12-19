c30b7ab3c3238352791a6064b63b2661cc645a7b
Priti Modanwal <99038209+ModanwalPriti06@users.noreply.github.com>
2022-12-19 10:26:35 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,10 +1,23 @@
 import React from "react"
 
 const AddTodo = ({ dispatch }) => {
-
+    const handleForm=(e)=>{
+        e.preventDefault();
+        let todoData={id:Date.now(),title:e.target.title.value}
+        dispatch({type:"ADD_TODO",payload:todoData})
+       // console.log(e.target.title.value);
+       e.target.reset();
+    }
 
     return (
-        <></>
+        <div className="form-wrapper">
+        <h1>Todo List UseReducer</h1>
+        <form onSubmit={handleForm}>
+            <input name="title" id="todo-form"/>
+            <button type="submit">Add</button>
+
+        </form>
+        </div>
     )
 }
 
